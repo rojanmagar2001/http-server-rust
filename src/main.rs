173f603225c06ba899b9b4a3fd31b007916ac3e1
Rojan Rana Magar <rozenmagar058@gmail.com>
2026-02-09 23:17:12 +0545

    Reply with 200 Status Code

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
-use std::net::TcpListener;
+use anyhow::Result;
+use log::{debug, error};
+use std::{io::Write, net::TcpListener};
 
-fn main() {
+fn main() -> Result<()> {
+    env_logger::init();
+
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                debug!("accepted new connection");
+
+                let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+                stream.write(response.as_bytes())?;
             }
             Err(e) => {
-                println!("error: {}", e);
+                error!("error: {}", e);
             }
         }
     }
+
+    Ok(())
 }
