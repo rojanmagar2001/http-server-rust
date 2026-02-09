ccc76673b9b5b3e21d4b1895a08e4cfcc67b9727
Rojan Rana Magar <rozenmagar058@gmail.com>
2026-02-09 23:55:52 +0545

    added 404 error

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use anyhow::Result;
-use log::{debug, error};
-use std::{io::Write, net::TcpListener};
+use log::{debug, error, info};
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::TcpListener,
+};
+
+const SUCCESS_RESPONSE: &[u8] = "HTTP/1.1 200 OK\r\n\r\n".as_bytes();
+const ERROR_RESPONSE: &[u8] = "HTTP/1.1 404 Not Found\r\n\r\n".as_bytes();
 
 fn main() -> Result<()> {
     env_logger::init();
+    info!("Server started");
 
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
@@ -12,9 +19,27 @@ fn main() -> Result<()> {
             Ok(mut stream) => {
                 debug!("accepted new connection");
 
-                let response = "HTTP/1.1 200 OK\r\n\r\n";
+                let mut request_buffer = BufReader::new(&stream);
+                let mut request_line = String::new();
 
-                stream.write(response.as_bytes())?;
+                request_buffer.read_line(&mut request_line)?;
+
+                let path: Vec<&str> = request_line.split_whitespace().collect();
+                match path[..] {
+                    ["GET", path, "HTTP/1.1"] => {
+                        if path == "/" {
+                            debug!("root path requested");
+                            stream.write(SUCCESS_RESPONSE)?;
+                        } else {
+                            debug!("unknown path: {path}");
+                            stream.write(ERROR_RESPONSE)?;
+                        }
+                    }
+                    _ => {
+                        error!("Invalid path in request. Input: `{request_line}`");
+                        stream.write(ERROR_RESPONSE)?;
+                    }
+                }
             }
             Err(e) => {
                 error!("error: {}", e);
