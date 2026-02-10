a757886507c4dd6ce4729d8c6bb198ce6903ef9f
Rojan Rana Magar <rozenmagar058@gmail.com>
2026-02-10 13:34:15 +0545

    Added User-Agent endpoint

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,9 @@ use std::{
 const SUCCESS_RESPONSE: &[u8] = "HTTP/1.1 200 OK\r\n\r\n".as_bytes();
 const ERROR_RESPONSE: &[u8] = "HTTP/1.1 404 Not Found\r\n\r\n".as_bytes();
 
+type Key = String;
+type Value = String;
+
 fn main() -> Result<()> {
     env_logger::init();
     info!("Server started");
@@ -24,6 +27,21 @@ fn main() -> Result<()> {
 
                 request_buffer.read_line(&mut request_line)?;
 
+                let mut headers: Vec<(Key, Value)> = Vec::new();
+
+                loop {
+                    let mut header_line = String::new();
+                    let next_header = request_buffer.read_line(&mut header_line)?;
+                    if header_line == "\r\n" || next_header == 0 {
+                        break;
+                    }
+                    if let Some((key, value)) = header_line.split_once(": ") {
+                        headers.push((key.to_string(), value.trim_end().to_string()));
+                    } else {
+                        error!("Invalid header line in request. Request: `{header_line}`");
+                    }
+                }
+
                 let path: Vec<&str> = request_line.split_whitespace().collect();
                 match path[..] {
                     ["GET", path, "HTTP/1.1"] => {
@@ -31,7 +49,34 @@ fn main() -> Result<()> {
                             debug!("root path requested");
                             stream.write(SUCCESS_RESPONSE)?;
                             stream.flush()?;
-                        } else if path.starts_with("/echo") {
+                        } else if path.starts_with("/user-agent/") {
+                            let mut user_agent = None;
+                            for (key, value) in headers {
+                                if key == "User-Agent" {
+                                    user_agent = Some(value);
+                                    break;
+                                }
+                            }
+
+                            match user_agent {
+                                None => {
+                                    error!(
+                                        "User-Agent header not found in request. Request: {request_line}"
+                                    );
+                                    stream.write(ERROR_RESPONSE)?;
+                                    stream.flush()?;
+                                }
+                                Some(user_agent) => {
+                                    let response = format!(
+                                        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                                        user_agent.len(),
+                                        user_agent,
+                                    );
+                                    stream.write(response.as_bytes())?;
+                                    stream.flush()?;
+                                }
+                            }
+                        } else if path.starts_with("/echo/") {
                             let echo_path = path.split_once("/echo/");
                             match echo_path {
                                 Some((_, path)) => {
@@ -47,7 +92,9 @@ fn main() -> Result<()> {
                                 _ => {
                                     error!(
                                         "Invalid echo path in request. Request: `{request_line}`"
-                                    )
+                                    );
+                                    stream.write(ERROR_RESPONSE)?;
+                                    stream.flush()?;
                                 }
                             }
                         } else {
